14b7a14d322617c3a1330a757a3ebf35203f7ea7
andrew <andrew.estudio@gmail.com>
2012-01-14 18:17:52 +0900

    before apply try catch in chcek status

--- src/com/andrew/agrieye/picnic/PicnicConfig.java
+++ src/com/andrew/agrieye/picnic/PicnicConfig.java
@@ -11,14 +11,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.UnknownHostException;
-import android.content.SharedPreferences;
 
 public class PicnicConfig {
 	private String ipAddress;
-	public String currentIpAddress;
 	private int udpPortP;
 	private int udpPortS;
 	private Picnic pic;
+	private String result;
 	
 	// constructor for set Ip Address
 	public PicnicConfig(String theIpAddress) {
@@ -52,21 +51,17 @@ public class PicnicConfig {
 		}
 	}
 	
-	// void method to connect to picnic using load library picnic
+	// void method to connect to picnic
 	public boolean connect(){
-		//currentIpAddress = getIpAddress.getString("ip","192.168.64.44");
-		currentIpAddress ="192.168.64.44";
 		udpPortP = 10001; // static
 		udpPortS = 10002; // static
 		try {
-			pic = new Picnic(currentIpAddress, udpPortP,udpPortS);			
+			pic = new Picnic(ipAddress,udpPortP, udpPortS);			
 			return true;
 		} catch (UnknownError e) {
-			// TODO: handle exception
 			e.printStackTrace();
 			return false;
 		}	
-
 	}
 	
 	// check status channel 1 - 7 
@@ -74,8 +69,7 @@ public class PicnicConfig {
 		// ---- check status button
 		if(connect() == true) {	
 			byte rb;
-			rb = pic.getRB();
-		
+			rb = pic.getRB();	
 			// convert to long, avoid the negative value 
 			long rbLong = rb & 0xFF;
 			
@@ -107,9 +101,7 @@ public class PicnicConfig {
 			statCh[4] = endResult.charAt(3);
 			statCh[5] = endResult.charAt(2);
 			statCh[6] = endResult.charAt(1);
-			statCh[7] = endResult.charAt(0);
-			
-		
+			statCh[7] = endResult.charAt(0);	
 			if(statCh[channel] == 1) {
 				return true;
 			} else {
@@ -118,25 +110,25 @@ public class PicnicConfig {
 		} else {
 			return false;
 		} 
-			
-		}
+	}
 	// method to turn on
 	public void turnOn(int channel){
-		connect();
-		pic.setHigh("RB", channel);
+		if(connect() == true ) {
+			pic.setHigh("RB", channel);
+			result = "ON";
+		} else {
+			result = "OFF";
+		}
 	}
 	
 	// method to turn off
 	public void turnOff(int channel){
-		connect();
-		pic.setLow("RB", channel);
-	}
-	
-	// function to print hello world;
-	public String helloSaja() {
-		String hello;
-		hello = "coba saja";
-		return hello;
+		if(connect() == true) {
+			pic.setLow("RB", channel);
+			result = "OFF";
+		} else {
+			result = "ON";
+		}
 	}
 	
 }
